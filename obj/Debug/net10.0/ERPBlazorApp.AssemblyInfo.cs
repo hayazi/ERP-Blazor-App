e67303f06d8df54261ed33a26ab074c11c9db3be
h_ayazi <h_ayazi@Processingworld.com>
2026-08-24 17:15:17 +0330

    add collaps and expand button in Department and Chart of Account Pages

--- obj/Debug/net10.0/ERPBlazorApp.AssemblyInfo.cs
+++ obj/Debug/net10.0/ERPBlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ERPBlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cee6332085e0537b5ee1b9e16f695afaafd058f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44327d2df96611025d67b94064595b8f576ef5b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ERPBlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ERPBlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
